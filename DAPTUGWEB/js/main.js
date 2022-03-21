3e90cda757a0190e10f4cb645d4c818df11800e6
tamnguen662001 <tam.nlt.61cntt@ntu.edu.vn>
2022-03-21 08:25:25 +0700

    Add project files.

---
+++ DAPTUGWEB/js/main.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    // $(window).on('scroll', function () {
+    //     if ($(this).scrollTop() > 1335) {
+    //         $('#x').addClass('fixed-top');
+    //     } else {
+    //         $('#x').removeClass('fixed-top');
+    //     }
+    // });
+    pos = $('#x').position();
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > parseInt(pos.top)) {
+            $('#x').addClass('fixed-top');
+            // $(".fixed-top").css(" transition" ,"all 0.3s ease-in-out");
+        } else {
+            $('#x').removeClass('fixed-top');
+        }
+    });
+
+
+
+
+    $(function () {
+        var $cart = $('#cart');
+        $('#clickme').click(function (e) {
+            e.stopPropagation();
+            if ($cart.is(":hidden")) {
+                $cart.slideDown("slow");
+            } else {
+                $cart.slideUp("slow");
+            }
+        });
+        $(document.body).click(function () {
+            if ($cart.not(":hidden")) {
+                $cart.slideUp("slow");
+            }
+        });
+    });
+})
+                      
