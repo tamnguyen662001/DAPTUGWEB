337bf47fc2da2a01307187dbba5ba76d2b39de48
tamnguen662001 <tam.nlt.61cntt@ntu.edu.vn>
2022-04-06 15:16:08 +0700

    do an phat trien ung dung web 6/4/2022

---
+++ DAPTUGWEB/Controllers/NguoiDungController.cs
@@ -0,0 +1,36 @@
+﻿using DAPTUGWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAPTUGWEB.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        ASP_QUAN_LY_SHOP_GIAYEntities db = new ASP_QUAN_LY_SHOP_GIAYEntities();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(KHACHHANG a)
+        {
+            db.KHACHHANGs.Add(a);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
