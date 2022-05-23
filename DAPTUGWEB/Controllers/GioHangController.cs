65e7206e828545ecb1f81639c80683165237f0f2
tamnguen662001 <tam.nlt.61cntt@ntu.edu.vn>
2022-05-23 20:41:10 +0700

    xay dung gio hang 23/5

---
+++ DAPTUGWEB/Controllers/GioHangController.cs
@@ -0,0 +1,136 @@
+﻿using DAPTUGWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAPTUGWEB.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        ASP_QUAN_LY_SHOP_GIAYEntities db = new ASP_QUAN_LY_SHOP_GIAYEntities();
+
+
+
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> dssp = Session["GioHang"] as List<GioHang>;
+            if (dssp == null)
+            {
+                dssp = new List<GioHang>;
+                Session["GioHang"] = dssp;
+
+            }
+            return dssp;
+        }
+
+        // them gio hang
+        public ActionResult ThemGioHang(string maSP, string url)
+        {
+            SANPHAM sp = db.SANPHAMs.SingleOrDefault(n => n.MASP == maSP);
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+
+            }
+            List<GioHang> dssp = LayGioHang();
+            GioHang gh = dssp.Find(n => n.maSP == maSP);
+            if (gh == null)
+            {
+                gh = new GioHang(maSP);
+                return Redirect(url);
+
+            }
+            else
+            {
+                gh.soLuong++;
+                return Redirect(url);
+
+            }
+
+        }
+
+        // cap nhat
+        public ActionResult CapNhatGioHang(string maSP, FormCollection f)
+        {
+            SANPHAM sp = db.SANPHAMs.SingleOrDefault(n => n.MASP == maSP);
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+
+            }
+            List<GioHang> dssp = LayGioHang();
+            GioHang gh = dssp.SingleOrDefault(n => n.maSP == maSP);
+            if (gh != null)
+            {
+                gh.soLuong = int.Parse(f["textSL"].ToString());
+
+
+            }
+            return View("GioHang");
+        }
+
+        // xoa gio hang
+        public ActionResult XoaGioHang(string maSP)
+        {
+            SANPHAM sp = db.SANPHAMs.SingleOrDefault(n => n.MASP == maSP);
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+
+            }
+            List<GioHang> dssp = LayGioHang();
+            GioHang gh = dssp.SingleOrDefault(n => n.maSP == maSP);
+            if (gh != null)
+            {
+                dssp.RemoveAll(n => n.maSP == gh.maSP);
+
+            }
+            if(dssp.Count == 0)
+            {
+                return RedirectToAction("TrangChu", "Home");
+            }
+            return RedirectToAction("GioHang");
+
+        }
+        public ActionResult GioHang()
+        {
+           
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("TrangChu", "Home");
+            }
+            List<GioHang> dssp = LayGioHang();
+            return View(dssp);
+            
+        }
+        private int TongSoLuong()
+        {
+            int TongSL = 0;
+            List<GioHang> dssp = Session["GioHang"] as List<GioHang>;
+            if (dssp != null)
+            {
+
+                TongSL = dssp.Sum(n => n.soLuong);
+            }
+            return TongSL;
+
+        }
+        private double TongTien()
+        {
+            double TongTien = 0;
+            List<GioHang> dssp = Session["GioHang"] as List<GioHang>;
+            if (dssp != null)
+            {
+
+                TongTien = dssp.Sum(n => n.thanhTien);
+            }
+            return TongTien;
+
+        }
+    }
+}
---
+++ DAPTUGWEB/Models/GioHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAPTUGWEB.Models
+{
+    public class GioHang
+    {
+        public string maSP { get; set; }
+
+        public string tenSP { get; set; }
+        public double donGia { get; set; }
+        public int soLuong { get; set; }
+        public string anhSP { get; set; }
+        public double thanhTien
+        {
+            get { return soLuong * donGia; }
+        }
+
+        ASP_QUAN_LY_SHOP_GIAYEntities db = new ASP_QUAN_LY_SHOP_GIAYEntities();
+        public GioHang(string MaSP)
+        {
+            maSP = MaSP;
+            SANPHAM sp = db.SANPHAMs.Single(n => n.MASP == maSP);
+            tenSP = sp.TENSP;
+            donGia = double.Parse(sp.DONGIA.ToString());
+            soLuong = 1;
+            anhSP = sp.ANHSP;
+
+        }
+    }
+}
